fc214f7391735f8e032b51b786cc2af62c4ad23e
Hannu Salo <hapsal@utu.fi>
2024-09-27 12:31:46 +0300

    feat: added footer

--- src/app/footer/footer.js
+++ src/app/footer/footer.js
@@ -3,9 +3,9 @@ import styles from "./footer.module.css"
 const Footer = () => {
     return (
         <footer className={styles.footer}>
-            <nav className={styles.content}>
-    
-            </nav>
+            <section className={styles.copyright}>
+                <p>&copy; Hannu Salo</p>
+            </section>
         </footer>
     )
 }
